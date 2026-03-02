9615903c7d147647e024cd5063efcfd3ef196e89
Arctic <arcticdev@arcticgaming.dev>
2026-03-01 23:11:55 -0500

    Added Capability Types

---
+++ Juniper.Core/Capabilities/CapabilityType.cs
@@ -0,0 +1,27 @@
+﻿public enum CapabilityType
+{
+    // AI
+    LLM,
+    STT,
+    WakeWord,
+    SpeakerDiarization,
+
+    // Audio
+    TTS,
+    Microphone,
+    Speaker,
+
+    // Vision
+    Camera,
+    
+    // Storage
+    Archive,
+
+    // Network
+    SyncHub,
+    APIGateway,
+
+    // System
+    AppResign,
+    Biometric,
+}
