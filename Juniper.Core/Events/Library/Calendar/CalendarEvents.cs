a7113f33ef152442759cfdba6dd40e10ae5f6a78
Arctic <arcticdev@arcticgaming.dev>
2026-03-03 22:26:11 -0500

    Added CalendarEvents and Payloads for Calendar stuff

---
+++ Juniper.Core/Events/Library/Calendar/CalendarEvents.cs
@@ -0,0 +1,53 @@
+﻿using Juniper.Core.Events.System;
+
+namespace Juniper.Core.Events.Library.Calendar;
+
+public static class CalendarEventTypes
+{
+    public const string Created = "calendar.entry.created";
+    public const string Updated = "calendar.entry.updated";
+    public const string Deleted = "calendar.entry.deleted";
+}
+
+public sealed record CalendarEntryCreatedEvent : IEvent<CalendarEntryUpsertPayload>
+{
+    public Guid EventId { get; init; }
+    public DateTimeOffset Timestamp { get; init; }
+    public Guid SourceDeviceId { get; init; }
+    public string EventType { get; init; } = CalendarEventTypes.Created;
+
+    public CalendarEntryUpsertPayload Payload { get; init; } = new();
+}
+
+public sealed record CalendarEntryUpdatedEvent : IEvent<CalendarEntryUpsertPayload>
+{
+    public Guid EventId { get; init; }
+    public DateTimeOffset Timestamp { get; init; }
+    public Guid SourceDeviceId { get; init; }
+    public string EventType { get; init; } = CalendarEventTypes.Updated;
+
+    public CalendarEntryUpsertPayload Payload { get; init; } = new();
+}
+
+public sealed record CalendarEntryDeletedEvent : IEvent<CalendarEntryDeletedPayload>
+{
+    public Guid EventId { get; init; }
+    public DateTimeOffset Timestamp { get; init; }
+    public Guid SourceDeviceId { get; init; }
+    public string EventType { get; init; } = CalendarEventTypes.Deleted;
+
+    public CalendarEntryDeletedPayload Payload { get; init; } = new();
+}
+
+// Payloads
+
+public sealed record CalendarEntryUpsertPayload
+{
+    public Guid CalendarEntryId { get; init; }
+    public Juniper.Core.Calendar.CalendarEntry Entry { get; init; } = new();
+}
+
+public sealed record CalendarEntryDeletedPayload
+{
+    public Guid CalendarEntryId { get; init; }
+}
